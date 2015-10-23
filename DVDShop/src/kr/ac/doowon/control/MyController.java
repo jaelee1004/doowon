ea5daa17c5eab2448f8c2724e54aee1355e03a92
jaelee1004 <jaelee1004@naver.com>
2015-10-23 17:14:22 +0900

    프로젝트시작

---
+++ DVDShop/src/kr/ac/doowon/control/MyController.java
@@ -0,0 +1,7 @@
+package kr.ac.doowon.control;
+
+public class MyController {
+   public void action(){
+	   System.out.println("뷰를 보여주세요~!!");
+   }
+}
---
+++ DVDShop/src/kr/ac/doowon/model/Model.java
@@ -0,0 +1,7 @@
+package kr.ac.doowon.model;
+
+public class Model {
+   public void m1(){
+	   System.out.println("¾È³ç~!!");
+   }
+}
